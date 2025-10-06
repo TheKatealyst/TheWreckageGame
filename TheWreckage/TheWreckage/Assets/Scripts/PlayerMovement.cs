3baa0c35522aac0b38e065f61d18173a3a65ac2d
Katelyn <donutknife99@gmail.com>
2025-10-06 21:46:30 +0200

    Made the movement mechanics.

---
+++ TheWreckage/TheWreckage/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    [SerializeField]
+    private float maxSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMousePositionDelayed(maxSpeed);
+    }
+
+    private void FollowMousePosition()
+    {
+        transform.position = GetWorldPositionFromMouse();
+    }
+
+    private void FollowMousePositionDelayed(float maxSpeed)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, GetWorldPositionFromMouse(), maxSpeed * Time.deltaTime);
+    }
+
+    private Vector3 GetWorldPositionFromMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Mathf.Abs(mainCamera.transform.position.z - transform.position.z);
+        return mainCamera.ScreenToWorldPoint(mousePos);
+    }
+}
