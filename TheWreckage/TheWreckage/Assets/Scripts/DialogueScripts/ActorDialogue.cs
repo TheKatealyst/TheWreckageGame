765bc04b09439922e5b1d312aa2f1c6f62395b8a
Katelyn <donutknife99@gmail.com>
2025-10-20 15:03:20 +0200

    Button Navigation & Dialogue triggerzone functionality added.

--- TheWreckage/TheWreckage/Assets/Scripts/DialogueScripts/ActorDialogue.cs
+++ TheWreckage/TheWreckage/Assets/Scripts/DialogueScripts/ActorDialogue.cs
@@ -13,9 +13,18 @@ public class Actor : MonoBehaviour
         }
     }
 
-    // Trigger dialogue for this actor
+    //Test function to Trigger dialogue for this actor by pressing space.
     public void SpeakTo()
     {
         DialogueManager.Instance.StartDialogue(Name, Dialogue.RootNode);
+    }
+    //Actual Trigger which can be set to this specific zone.
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered has the tag "Player"
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SpeakTo();
+        }
     }
 }
